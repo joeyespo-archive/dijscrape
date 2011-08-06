0381b1bc3743af871c34d326eb6bd9fbccb119bf
Joe Esposito <joe@joeyespo.com>
2011-08-06 02:20:55 -0400

    Added Gmail OAuth functionality.

--- config.py
+++ config.py
@@ -11,13 +11,23 @@ DevelopmentConfig.DEV_PORT = 80
 
 class BaseConfig(object):
     ADMINS = ['espo58@gmail.com']
-    DATABASE_URI = 'sqlite://:memory:'
     DEBUG = False
+    DATABASE_URI = ''
+    OAUTH_SCOPE_URL = 'https://mail.google.com/'
+    OAUTH_REQUEST_TOKEN_URL = 'https://www.google.com/accounts/OAuthGetRequestToken'
+    OAUTH_AUTHORIZATION_URL = 'https://www.google.com/accounts/OAuthAuthorizeToken'
+    OAUTH_ACCESS_TOKEN_URL = 'https://www.google.com/accounts/OAuthGetAccessToken'
+    # Override these in your local config file
+    APP_SECRET_KEY = ''
+    OAUTH_CONSUMER_KEY = ''
+    OAUTH_CONSUMER_SECRET = ''
 
 class DevelopmentConfig(BaseConfig):
     DEV_HOST = 'localhost'
     DEV_PORT = 5000
     DEBUG = True
+    DATABASE_URI = 'sqlite://:memory:'
+    APP_SECRET_KEY = 'development-key'
 
 class ProductionConfig(BaseConfig):
     DATABASE_URI = 'mysql://dijscrape@localhost/dijscrape'
--- dijscrape.py
+++ dijscrape.py
@@ -1,7 +1,10 @@
 #!/usr/bin/env python
 
 import os
-from flask import Flask, render_template
+import cgi
+import oauth2
+from flask import Flask, render_template, abort, request, session, redirect, url_for, flash
+from flaskext.oauth import OAuth
 
 # Get config class name based on context
 config_class_name = 'Development' if __name__ == '__main__' else 'Production'
@@ -9,25 +12,52 @@ config_class_name = 'Development' if __name__ == '__main__' else 'Production'
 # Flask application
 app = Flask(__name__)
 app.config.from_object('config.%sConfig' % config_class_name)
+app.secret_key = app.config['APP_SECRET_KEY']
+
+# TODO: Move this part elsewhere
+consumer = oauth2.Consumer(app.config['OAUTH_CONSUMER_KEY'], app.config['OAUTH_CONSUMER_SECRET'])
+client = oauth2.Client(consumer)
 
 # Views
 @app.route('/')
 def index():
     return render_template('index.html')
 
-@app.route('/scrape')
-def scrape():
+@app.route('/login')
+def login():
+    resp, content = client.request(app.config['OAUTH_REQUEST_TOKEN_URL'] + '?scope=' + app.config['OAUTH_SCOPE_URL'], "GET")
+    if resp['status'] != '200':
+        abort(502, 'Invalid response from Google.')
+    session['request_token'] = dict(cgi.parse_qsl(content))
+    return redirect('%s?oauth_token=%s&oauth_callback=http://%s%s'
+        % (app.config['OAUTH_AUTHORIZATION_URL'], session['request_token']['oauth_token'], request.host, url_for('oauth_authorized')))
+
+@app.route('/oauth-scraper')
+def oauth_scraper():
     # TODO: Handle email scraping
-    return 'TODO: implement'
+    return 'Connected!'
+
+# OAuth callbacks
+@app.route('/oauth-authorized')
+def oauth_authorized():
+    token = oauth2.Token(session['request_token']['oauth_token'], session['request_token']['oauth_token_secret'])
+    client = oauth2.Client(consumer, token)
+    resp, content = client.request(app.config['OAUTH_ACCESS_TOKEN_URL'], "GET")
+    # TODO: Handle 'Deny access' (status 400)
+    if resp['status'] != '200':
+        raise Exception("Invalid response from Google.")
+    access_token = dict(cgi.parse_qsl(content))
+    # TODO: Store the access token
+    return redirect(url_for('oauth_scraper'))
 
 # Error handlers
 @app.errorhandler(404)
-def page_not_found(error = None):
+def page_not_found(message = None):
     return render_template('error404.html'), 404
 
 @app.errorhandler(500)
 @app.route('/internal_error.html')
-def internal_error(error = None):
+def internal_error(message = None):
     return render_template('error500.html'), 500
 
 # Run dev server
