759e69de061a154db99585eefcd800dbaad42484
Joe Esposito <joe@joeyespo.com>
2011-08-07 02:00:18 -0400

    Completed Gmail oauth functionality, ported the code from scraper.py to tasks.py, and added the results page.

--- dijscrape.py
+++ dijscrape.py
@@ -4,7 +4,7 @@ import os
 import cgi
 import oauth2 as oauth
 from flask import Flask, render_template, abort, request, session, redirect, url_for, flash
-from scraper import Scraper
+from tasks import scrape_gmail_messages
 
 # Get config class name based on context
 config_class_name = 'Development' if __name__ == '__main__' else 'Production'
@@ -13,42 +13,56 @@ config_class_name = 'Development' if __name__ == '__main__' else 'Production'
 app = Flask(__name__)
 app.config.from_object('config.%sConfig' % config_class_name)
 app.secret_key = app.config['APP_SECRET_KEY']
-
-# TODO: Move this part elsewhere
 consumer = oauth.Consumer(app.config['GOOGLE_KEY'], app.config['GOOGLE_SECRET'])
 client = oauth.Client(consumer)
+
 
 # Views
 @app.route('/')
 def index():
     return render_template('index.html')
 
+
 @app.route('/login')
 def login():
-    resp, content = client.request(app.config['OAUTH_REQUEST_TOKEN_URL'] + '?scope=' + app.config['OAUTH_SCOPE_URL'], "GET")
+    email = request.args.get('email')
+    if not email:
+        flash('Please enter a valid email.')
+        return redirect('/')
+    # TODO: Multiple scopes? See: http://code.google.com/apis/accounts/docs/OAuth_ref.html#SigningOAuth
+    resp, content = client.request(app.config['OAUTH_REQUEST_TOKEN_URL'] + '?scope=' + app.config['OAUTH_SCOPE_URL'])
     if resp['status'] != '200':
         abort(502, 'Invalid response from Google.')
     session['request_token'] = dict(cgi.parse_qsl(content))
     return redirect('%s?oauth_token=%s&oauth_callback=http://%s%s'
-        % (app.config['OAUTH_AUTHORIZATION_URL'], session['request_token']['oauth_token'], request.host, url_for('oauth_authorized')))
+        % (app.config['OAUTH_AUTHORIZATION_URL'], session['request_token']['oauth_token'], request.host, url_for('oauth_authorized', email=email)))
+
+
+@app.route('/oauth-authorized')
+def oauth_authorized():
+    email = request.args.get('email')
+    token = oauth.Token(session['request_token']['oauth_token'], session['request_token']['oauth_token_secret'])
+    client = oauth.Client(consumer, token)
+    resp, content = client.request(app.config['OAUTH_ACCESS_TOKEN_URL'])
+    # TODO: Handle 'Deny access' (status 400)
+    if resp['status'] != '200':
+        # TODO: Show better error
+        abort(502, 'Invalid response from Google.')
+    session['access_token'] = dict(cgi.parse_qsl(content))
+    return redirect(url_for('oauth_scraper', email=email))
+
 
 @app.route('/oauth-scraper')
 def oauth_scraper():
-    # TODO: Handle email scraping
-    return 'Connected!'
-
-# OAuth callbacks
-@app.route('/oauth-authorized')
-def oauth_authorized():
-    token = oauth.Token(session['request_token']['oauth_token'], session['request_token']['oauth_token_secret'])
-    client = oauth.Client(consumer, token)
-    resp, content = client.request(app.config['OAUTH_ACCESS_TOKEN_URL'], "GET")
-    # TODO: Handle 'Deny access' (status 400)
-    if resp['status'] != '200':
-        abort(502, 'Invalid response from Google.')
-    access_token = dict(cgi.parse_qsl(content))
-    # TODO: Store the access token
-    return redirect(url_for('oauth_scraper'))
+    email = request.args.get('email')
+    # TODO: Get the email from oauth -- need multiple scopes to do this: http://stackoverflow.com/questions/3128981/require-google-to-return-email-address-as-part-of-oauth
+    access_oauth_token, access_oauth_token_secret = session['access_token']['oauth_token'], session['access_token']['oauth_token_secret']
+    consumer_key, consumer_secret = app.config['GOOGLE_KEY'], app.config['GOOGLE_SECRET']
+    result = scrape_gmail_messages.delay(email, access_oauth_token, access_oauth_token_secret, consumer_key, consumer_secret)
+    # TODO: return render_template('processing.html')
+    phone_numbers = result.get()
+    return render_template('results.html', phone_numbers=phone_numbers)
+        
 
 # Error handlers
 @app.errorhandler(404)
---
+++ tasks.py
@@ -0,0 +1,94 @@
+import sys
+import re
+import datetime
+import oauth2 as oauth
+import oauth2.clients.imap as imaplib
+from email.parser import HeaderParser
+from dateutil.parser import parse
+from celery.decorators import task
+
+
+number_re = re.compile('(?:(?:\+?1\s*(?:[.-]\s*)?)?(?:\(\s*([2-9]1[02-9]|[2-9][02-8]1|[2-9][02-8][02-9])\s*\)|([2-9]1[02-9]|[2-9][02-8]1|[2-9][02-8][02-9]))\s*(?:[.-]\s*)?)?([2-9]1[02-9]|[2-9][02-9]1|[2-9][02-9]{2})\s*(?:[.-]\s*)?([0-9]{4})(?:\s*(?:#|x\.?|ext\.?|extension)\s*(\d+))?')
+email_re = re.compile('("?([a-zA-Z 0-9\._\-]+)"?\s+)?<?([a-zA-Z0-9\._\-]+@[a-zA-Z0-9\._\-]+)>?')
+
+
+@task()
+def find_phone_numbers(imap, number):
+    print 'Processing message', number
+    
+    # TODO: Handle exceptions and bad resp
+    resp, message_data = imap.fetch(number, '(BODY.PEEK[HEADER])')
+    raw_message = message_data[0][1] # message_data, the data structure returned by imaplib, encodes some data re: the request type
+    header = HeaderParser().parsestr(raw_message)
+    # Check for multipart message
+    if 'multipart' in (header['Content-Type'] or []):
+        # TODO: Handle multipart messages
+        print 'Skipping multipart message'
+        return []
+    
+    # TODO: Handle exceptions and bad resp
+    resp, message_data = imap.fetch(number, '(BODY.PEEK[TEXT])')
+    text_payload = message_data[0][1]
+    raw_phone_numbers = number_re.findall(text_payload) + number_re.findall(header['Subject'])
+    # TODO: Make this more clear (flattens ['','650','555','1212'] to a string)
+    raw_phone_numbers = list(set(map(''.join, raw_phone_numbers)))
+    print "Phone numbers (%s): %s" % (len(raw_phone_numbers), raw_phone_numbers)
+    if len(raw_phone_numbers) == 0:
+        return []
+    
+    date_timestamp = header['Date']
+    date = parse(date_timestamp)
+    print 'Header date:', date, '(', date_timestamp, ')'
+    
+    # This may fail due to unicode issues
+    try:
+        name, email = email_re.match(header['From']).groups()[1:3]
+        print 'From:', name, email
+    except:
+        from traceback import format_exc
+        print format_exc()
+        return []
+    
+    message = {
+        'sender': header['From'],
+        'recipient': header['To'],
+        'sender_name': name,
+        'sender_email': email,
+        'subject': header['Subject'],
+        'date_add': date,
+        'payload': text_payload,
+    }
+    
+    # TODO: Clean this up
+    for raw_phone_number in raw_phone_numbers:
+        # TODO: Handle numbers without area codes
+        if len(str(raw_phone_number)) <= 7:
+            continue
+        phone_numbers.append({'value': phone_number, 'message': message})
+    return phone_numbers
+
+
+@task()
+def scrape_gmail_messages(email, access_oauth_token, access_oauth_token_secret, consumer_key, consumer_secret):
+    url = 'https://mail.google.com/mail/b/%s/imap/' % email
+    consumer = oauth.Consumer(consumer_key, consumer_secret)
+    token = oauth.Token(access_oauth_token, access_oauth_token_secret)
+    imap = imaplib.IMAP4_SSL('imap.googlemail.com')
+    imap.authenticate(url, consumer, token)
+    
+    # TODO: Handle bad resp
+    resp, message_count = imap.select()
+    message_count = int(message_count[0])
+    print "Message count: %d" % message_count
+    
+    # TODO: Handle bad resp
+    resp, messages = imap.search(None, 'ALL')
+    messages = messages[0].split()
+    
+    # Find the phone numbers in each message
+    phone_numbers = []
+    for index in range(message_count):
+        phone_numbers += find_phone_numbers(imap, messages[index])
+    
+    imap.logout()
+    return phone_numbers
